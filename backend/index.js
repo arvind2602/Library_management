a16b2ff29ba7a431d783e621e50519e7a4dc66ba
arvind2602 <guptaarvind2602@gmail.com>
2024-10-07 22:20:13 +0530

    Added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import AuthRouter from './routes/auth.js';
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(bodyParser.json());
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
 })
+
+app.use('/auth', AuthRouter);
 
 app.use('/', (req, res) => {
     res.send('Hello World');
---
+++ backend/routes/auth.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import pool from '../utils/db.js';
+import Joi from 'joi';
+
+const AuthRouter = express.Router();
+
+AuthRouter.post('/register', async (req, res) => {
+    const schema = Joi.object({
+        username: Joi.string().min(3).max(255).required(),
+        password: Joi.string().min(4).max(255).required(),
+        role: Joi.string().min(3).max(255).required()
+    });
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).json(error.details[0].message);
+    }
+
+    try {
+        const { username, password, role } = req.body;
+        const newUser = await pool.query(
+            "INSERT INTO users (username, password,role) VALUES ($1, $2, $3) RETURNING *",
+            [username, password, role]
+        );
+        res.json(newUser.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+);
+
+
+// Login route
+AuthRouter.post('/login', async (req, res) => {
+    const schema = Joi.object({
+        username: Joi.string().min(3).max(255).required(),
+        password: Joi.string().min(4).max(255).required()
+    });
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).json(error.details[0].message);
+    }
+
+    // Check if user exists
+    const userExist = await pool.query("SELECT * FROM users WHERE username = $1", [req.body.username]);
+    if (userExist.rows.length === 0) {
+        return res.status(401).json('User does not exist');
+    }
+
+    try {
+        const { username, password } = req.body;
+        const user = await pool.query("SELECT * FROM users WHERE username = $1 AND password = $2", [username, password]);
+        if (user.rows.length === 0) {
+            return res.status(401).json('Invalid Credential');
+        }
+        res.json(user.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+export default AuthRouter;
